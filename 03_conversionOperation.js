d89ed4bb8d14f9659f9623ca2be72a56ae66dfd2
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-21 14:29:30 +0000

    Converson of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+// "33" => 33
+// true => 1 ; false => 0
+// "33asc" => NAN(not a number)
+let IsLoggedin = 1;
+
+let booleanIsLoggedin = Boolean(IsLoggedin)
+console.log(booleanIsLoggedin);
+
+// true => 1 ; false => 0
+// "" => false
+// "raju" => true
+
+let SomeNumber = 23
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
