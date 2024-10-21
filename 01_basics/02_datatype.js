f7d33beaadb86e01afd79d65e35b09cb624134b9
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-21 13:12:55 +0000

    container added

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -4,3 +4,19 @@
 
 console.log(3+3); console.log("debanjan");
 //code should be readable
+
+let Name="Debanjan"
+let age = 20
+let IsLoggedin= true
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => yes / no
+// Null => standalone value 
+// undifinded=> 
+// symbol => unique
+
+
+// object
+console.log(typeof "Debanjan");
