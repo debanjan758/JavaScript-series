7b511183e0bc860ed2bdc46bcc80d55d9565ce8e
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-21 12:26:52 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 567898
+let accoutEmail = "deanjansen32@gmail.com"
+var accountPassword = "ramuda456"
+accountCity = "kolkata"
+
+accoutEmail="ramudaa546@gmail.com"
+accountPassword="234567"
+accountCity="purulia"
+
+let accountState
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// accountId = 222 // change n0ot allowed
+console.log(accountId);
+console.table([accountId, accountPassword, accoutEmail, accountCity, accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all js codes as new version
+
+// alart(3+3) we are using node js not browser.
+
+console.log(3+3); console.log("debanjan");
+//code should be readable
