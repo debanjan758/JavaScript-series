69df9c045aacb90b6ff2952dcbca1ad961804b97
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-22 04:11:55 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// // "33" => 33
+// // true => 1 ; false => 0
+// // "33asc" => NAN(not a number)
+// let IsLoggedin = 1;
+
+// let booleanIsLoggedin = Boolean(IsLoggedin)
+// console.log(booleanIsLoggedin);
+
+// // true => 1 ; false => 0
+// // "" => false
+// // "raju" => true
+
+// let SomeNumber = 23
+
+// let StringNumber = String(SomeNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// *************************operations******************************
+
+console.log(1+2);
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);     // string + number => all will be converted in string
+console.log(1 + 2 + "2");     // number + string => all will be converted in numbmer
+
+
+
+
+
+
+
+
