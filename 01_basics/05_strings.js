850d4ecf1dcf0f36f9726cd4bb26ea29b0c8dba5
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-23 09:27:11 +0000

    string change01

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ console.log(name + repoCount + " valueee");
 
 console.log(`Hello my name is ${name} My repo Count is ${repoCount}`);
 
-const gameName = new String('Debanjan');
+const gameName = new String('Debanjan-guru');
 
 console.log(gameName[3]);
 console.log(gameName.__proto__);
@@ -16,5 +16,19 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(7));
 console.log(gameName.indexOf('b'));
 
+const newString = gameName.substring(0 , 4)
+console.log(newString);
 
+const anotherString = gameName.slice(-7 , 4)
+console.log(anotherString);
 
+const newStringOne = "    Debanjan    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = 'https://github.com/debanjan758/JavaScript-series/blob/main/01_basics/05_strings.js'
+console.log(url.replace('20%', '-'));
+
+console.log(url.includes('debanjan'));
+
+console.log(gameName.split('-'));
