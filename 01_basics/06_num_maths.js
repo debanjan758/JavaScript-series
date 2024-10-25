d7304d48bc36bb02f38c49ccaafef8ccc651a8fa
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-25 07:15:36 +0000

    math and num

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,37 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+// const anotherNumber = 345.877412
+// console.log(anotherNumber.toPrecision(3));
+
+// const hundrred = 100000000
+// console.log(hundrred.toLocaleString());
+// console.log(hundrred.toLocaleString('en-IN'));
+
+// ******************************MATHS***********************
+
+console.log(Math);
+console.log(Math.round(4.3));
+console.log(Math.abs(-5));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.min(4, 5, 2, 8));
+console.log(Math.max(4, 55, 44, 77));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
