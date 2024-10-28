1050bf57f870add4266809d38ff125a681fa1607
debanjan758 <146684848+debanjan758@users.noreply.github.com>
2024-10-28 14:13:37 +0000

    date and time

---
+++ 01_basics/07_dates_times.js
@@ -0,0 +1,20 @@
+// Dates...
+
+const myaDate = new Date()
+console.log(myaDate.toString());
+console.log(myaDate.toDateString());
+console.log(myaDate.toISOString());
+console.log(myaDate.toJSON());
+console.log(myaDate.toLocaleDateString());
+console.log(myaDate.toLocaleTimeString());
+
+console.log(typeof myaDate);
+
+const mycreatedDate = new Date(2024, 0, 23)
+console.log(mycreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+
+
